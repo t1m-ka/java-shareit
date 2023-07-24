7de9d98378bb3a9df2d0d586a991d7895390a867
Anatoly <aa.mitapov@yandex.ru>
2023-07-24 17:01:01 +0800

    feat: создание сервисных классов

---
+++ src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -0,0 +1,34 @@
+package ru.practicum.shareit.item;
+
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.List;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+    @Override
+    public Item addItem(Item item, long userId) {
+        return null;
+    }
+
+    @Override
+    public Item updateItem(Item item, long userId) {
+        return null;
+    }
+
+    @Override
+    public Item getItem(long itemId) {
+        return null;
+    }
+
+    @Override
+    public List<Item> getItemsListByOwner(long userId) {
+        return null;
+    }
+
+    @Override
+    public List<Item> searchItem(String text) {
+        return null;
+    }
+}
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.user;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
 /**
  * TODO Sprint add-controllers.
  */
+
+@Component
+@Data
 public class User {
+    private long id;
+
+    private String name;
+
+    private String email;
 }
