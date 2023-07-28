97c6020bc50b468e16521c8fd8e6faf1dd468052
Anatoly Mitapov <aa.mitapov@yandex.ru>
2023-07-28 21:18:17 +0800

    feat: создание класса ItemRequest

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,16 @@
 package ru.practicum.shareit.request;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
+@Component
+@Data
 public class ItemRequest {
+    private long id;
+    private String description;
+    private User requestor;
+    private LocalDateTime created;
 }
